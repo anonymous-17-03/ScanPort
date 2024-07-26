cea62b2db45eeb0f2c4f8a7e9d981973cba25354
anonymous-17-03 <victordgarciap2003@gmail.com>
2024-07-26 00:50:32 -0500

    v.1

--- PortScanner.py
+++ PortScanner.py
@@ -1,18 +1,23 @@
 import socket
 import sys
 
+open_ports = []
+
 def escanear_puertos(objetivo):
-    print("[+] Escaneando la máquina host:", objetivo)
+    print("\n[+] Targeted:", objetivo, "\n")
 
     try:
         for port in range(1, 65536):
-            print("[+] Port:", port, "/ 65535 ", end="\r")
+            print("  [+] Port:", port, "/ 65535 ", end="\r")
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             socket.setdefaulttimeout(1)
             resultado = s.connect_ex((objetivo, port))
             if resultado == 0:
-                print("  [!] {} Port abierto".format(port))
+                print("  [!] Puerto {} abierto".format(port))
+                open_ports.append(port)
             s.close()
+            if port == 65535:
+                print("\n\n[+] Lista de puertos abiertos:", open_ports, "\n")
 
     except KeyboardInterrupt:
         print("\n[!] Escaneo interrumpido por el usuario.\n")
