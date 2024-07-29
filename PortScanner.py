e8bfbd45841b1976a2aa7f951da1a977ff3dec97
anonymous-17-03 <victordgarciap2003@gmail.com>
2024-07-29 18:22:20 -0500

    v.4.0.0

--- PortScanner.py
+++ PortScanner.py
@@ -33,7 +33,73 @@ def get_os(ttl):
     else:
         return "Not Found"
 
+def check_ftp_vulnerability(ip, port, file):
+    try:
+        proc = subprocess.Popen(["nmap", "-p", str(port), "--script", "ftp-anon", ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+        if b'Anonymous FTP login allowed' in out:
+            print(colored.fg('red') + "  [!] FTP Vulnerable: Login como anonymous exitoso" + colored.attr('reset'))
+            file.write(colored.fg('red') + "  [!] FTP Vulnerable: Login como anonymous exitoso" + colored.attr('reset') + "\n")
+        else:
+            print(colored.fg('green') + "  [+] FTP parece seguro contra login anonymous" + colored.attr('reset'))
+            file.write(colored.fg('green') + "  [+] FTP parece seguro contra login anonymous" + colored.attr('reset') + "\n")
+    except Exception as e:
+        print(colored.fg('red') + f"\n  [!] Error comprobando vulnerabilidad FTP: {str(e)}\n" + colored.attr('reset'))
+
+def check_ssh_vulnerability(ip, port, file):
+    try:
+        proc = subprocess.Popen(["nmap", "-sCV", "-p", str(port), ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+        if b'OpenSSH' in out:
+            version_line = next(line for line in out.split(b'\n') if b'OpenSSH' in line)
+            version_match = re.search(rb'OpenSSH\s(\d+\.\d+)', version_line)
+            if version_match:
+                version = version_match.group(1).decode('utf-8')
+                version_number = float(version)
+                if version_number <= 7.7:
+                    print(colored.fg('red') + "  [!] SSH Vulnerable: Enumeración de usuarios " + colored.attr('reset'))
+                    file.write(colored.fg('red') + "  [!] SSH Vulnerable: Enumeración de usuarios " + colored.attr('reset') + "\n")
+                else:
+                    print(colored.fg('green') + "  [+] SSH parece seguro contra enumeración de usuarios  " + colored.attr('reset'))
+                    file.write(colored.fg('green') + "  [+] SSH parece seguro contra enumeración de usuarios  " + colored.attr('reset') + "\n")
+            else:
+                print(colored.fg('yellow') + "  [!] No se pudo determinar la versión de SSH" + colored.attr('reset'))
+                file.write(colored.fg('yellow') + "  [!] No se pudo determinar la versión de SSH" + colored.attr('reset') + "\n")
+        else:
+            print(colored.fg('yellow') + "  [!] No se pudo determinar la versión de SSH" + colored.attr('reset'))
+            file.write(colored.fg('yellow') + "  [!] No se pudo determinar la versión de SSH" + colored.attr('reset') + "\n")
+    except Exception as e:
+        print(colored.fg('red') + f"  [!] Error comprobando vulnerabilidad SSH: {str(e)}" + colored.attr('reset'))
+        file.write(colored.fg('red') + f"  [!] Error comprobando vulnerabilidad SSH: {str(e)}" + colored.attr('reset') + "\n")
+
+def check_http_technologies(ip, port, file):
+    try:
+        proc = subprocess.Popen(["whatweb", f"http://{ip}:{port}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate(timeout=10)
+        if proc.returncode == 0:
+            output = out.decode('utf-8').strip()
+            if output:
+                items = [item.strip() for item in output.split(',')]
+                formatted_output = "\n".join(f"        {item}," for item in items)
+                print(colored.fg('48') + "  [+] Tecnologías detectadas en HTTP:\n" + colored.attr('reset'))
+                file.write(colored.fg('48') + "  [+] Tecnologías detectadas en HTTP:\n" + colored.attr('reset') + "\n")
+                print(formatted_output)
+                file.write(formatted_output + "\n\n")
+            else:
+                print(colored.fg('red') + "  [!] Error: No se detectaron tecnologías HTTP." + colored.attr('reset'))
+                file.write(colored.fg('red') + "  [!] Error: No se detectaron tecnologías HTTP." + colored.attr('reset') + "\n")
+        else:
+            print(colored.fg('red') + "  [!] Error: No se pudieron detectar tecnologías HTTP." + colored.attr('reset'))
+            file.write(colored.fg('red') + "  [!] Error: No se pudieron detectar tecnologías HTTP." + colored.attr('reset') + "\n")
+    except subprocess.TimeoutExpired:
+        print(colored.fg('red') + "  [!] Error: Tiempo de espera excedido para detectar tecnologías HTTP." + colored.attr('reset'))
+        file.write(colored.fg('red') + "  [!] Error: Tiempo de espera excedido para detectar tecnologías HTTP." + colored.attr('reset') + "\n")
+    except Exception as e:
+        print(colored.fg('red') + f"  [!] Error detectando tecnologías HTTP: {str(e)}" + colored.attr('reset'))
+        file.write(colored.fg('red') + f"  [!] Error detectando tecnologías HTTP: {str(e)}" + colored.attr('reset') + "\n")
+
 def escanear_puertos(objetivo, file):
+
     print(colored.fg('green') + "\n[+] Autor: " + colored.fg('blue') + "Anonymous17" + colored.attr('reset'))
     print(colored.fg('green') + "[+] Targeted: " + colored.fg('red') + objetivo + colored.attr('reset'))
     print(colored.fg('green') + "[+] (TTL -> %s): " % (ttl) + colored.fg('yellow') + "%s" % os_name + colored.attr('reset'))
@@ -57,14 +123,20 @@ def escanear_puertos(objetivo, file):
         if open_ports:
             print(colored.fg('magenta') + "\n\n[+] Lista de puertos abiertos:" + colored.fg('48'), open_ports)
             file.write(colored.fg('magenta') + "\n[+] Lista de puertos abiertos:" + colored.fg('48') + " {}\n".format(open_ports))
-
-        # Mostrar servicios y versiones
         if open_ports:
             print(colored.fg('cyan') + "[+] Información de servicios y versiones:\n" + colored.attr('reset'))
             file.write(colored.fg('cyan') + "[+] Información de servicios y versiones:\n\n" + colored.attr('reset'))
             for port in open_ports:
                 get_service_info(objetivo, port, file)
-
+        print(colored.fg('cyan') + "\n[+] Comprobando vulnerabilidades:\n" + colored.attr('reset'))
+        file.write(colored.fg('cyan') + "\n[+] Comprobando vulnerabilidades:\n\n" + colored.attr('reset'))
+        for port in open_ports:
+            if port == 21:
+                check_ftp_vulnerability(objetivo, port, file)
+            elif port == 22:
+                check_ssh_vulnerability(objetivo, port, file)
+            elif port in [80, 8080, 8000, 443, 8443]:
+                check_http_technologies(objetivo, port, file)
     except KeyboardInterrupt:
         print(colored.fg('red') + "\n\n[!] Saliendo...\n" + colored.attr('reset'))
         sys.exit(0)
@@ -85,7 +157,6 @@ def get_service_info(ip, port, file):
         output_lines = out.decode('utf-8').split('\n')
         for line in output_lines:
             if '/tcp' in line:
-                # Remover 'open' y formatear según los colores especificados
                 line = line.replace('open', '').strip()
                 if line:
                     parts = line.split()
@@ -93,14 +164,11 @@ def get_service_info(ip, port, file):
                         port_info = parts[0]
                         service_info = parts[1]
                         version_info = ' '.join(parts[2:])
-                        # Formatear con alineación
                         formatted_line = f"{port_info:<8} {service_info:<8} {version_info}"
                         print(colored.fg('blue') + "  [+] " + colored.fg('yellow') + formatted_line[:8] + " " + colored.fg('red') + formatted_line[8:16] + " " + colored.fg(48) + formatted_line[16:] + colored.attr('reset'))
                         file.write(colored.fg('blue') + "  [+] " + colored.fg('yellow') + formatted_line[:8] + " " + colored.fg('red') + formatted_line[8:16] + " " + colored.fg(48) + formatted_line[16:] + colored.attr('reset') + "\n")
     except Exception as e:
         print(colored.fg('red') + f"\n[!] Error: {str(e)}\n" + colored.attr('reset'))
-
-
 
 if __name__ == "__main__":
 
@@ -112,12 +180,9 @@ if __name__ == "__main__":
     ttl = get_ttl(ip_address)
     os_name = get_os(ttl)
     open_ports = []
-
     objetivo = socket.gethostbyname(sys.argv[1])
     output_file = 'Targeted.txt'
     with open(output_file, 'w') as file:
         escanear_puertos(objetivo, file)
-
-    # Mensaje final
-    print(colored.fg('green') + f"\n[*] Evidencia guardada en" + colored.fg('red') + f" {output_file}" + colored.attr('reset'))
+    print(colored.fg('green') + f"\n[*] Evidencia guardada en" + colored.fg('red') + f" {output_file}" + colored.attr('reset') + "\n")
 
--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # PortScanner
 
-Este es un script de Python para escanear puertos abiertos en una dirección IP objetivo y obtener información sobre los servicios y versiones que se están ejecutando en esos puertos. Además, el script detecta el sistema operativo de la máquina objetivo basándose en el valor TTL (Time to Live).
+Este es un script de Python para escanear puertos abiertos en una dirección IP objetivo y obtener información sobre los servicios y versiones que se están ejecutando en esos puertos. Además, el script detecta el sistema operativo de la máquina objetivo basándose en el valor TTL (Time to Live). También verifica vulnerabilidades comunes en los servicios de FTP, SSH y HTTP.
 
 ## Características
 
 - Escaneo de puertos del 1 al 65535.
 - Detección del sistema operativo objetivo (Linux o Windows) basado en TTL.
 - Información de servicios y versiones utilizando `nmap`.
+- Comprobación de vulnerabilidades en servicios de FTP y SSH.
+- Detección de tecnologías HTTP utilizando `whatweb`.
 - Salida de información formateada y coloreada en la terminal.
 - Guardado de la evidencia en un archivo `Targeted.txt`.
 
@@ -20,7 +22,8 @@ Asegúrate de tener instaladas las siguientes herramientas en tu sistema:
 - `subprocess`
 - `sys`
 - `colored`
-- nmap
+- `nmap`
+- `whatweb`
 
 ## Instalación
 
@@ -38,6 +41,12 @@ Asegúrate de tener instaladas las siguientes herramientas en tu sistema:
     pip install colored
     ```
 
+3. Asegúrate de tener instalados `nmap` y `whatweb` en tu sistema. Puedes instalarlos usando el gestor de paquetes de tu sistema operativo. Por ejemplo, en Debian/Ubuntu puedes usar:
+
+    ```bash
+    sudo apt-get install nmap whatweb
+    ```
+
 ## Uso
 
 Ejecuta el script con el siguiente comando, reemplazando `<objetivo>` con la dirección IP que deseas escanear:
@@ -47,12 +56,13 @@ python3 PortScanner.py <objetivo>
 ```
 
 ## Ejemplo de Salida
+
 A continuación, se muestra un ejemplo de cómo se ve la salida del script al ejecutarlo:
 
 ![Resultado con nmap](img.png)
 
 ## Notas
 
-Este script solo funciona en sistemas Unix-like debido al uso del comando ping con la opción -c.
-Asegúrate de tener permisos de red adecuados para ejecutar pings y escanear puertos en el objetivo especificado.
-Usa este script de manera ética y solo en sistemas sobre los que tengas permiso de realizar escaneos de red.
+- Este script solo funciona en sistemas Unix-like debido al uso del comando ping con la opción -c.
+- Asegúrate de tener permisos de red adecuados para ejecutar pings y escanear puertos en el objetivo especificado.
+- Usa este script de manera ética y solo en sistemas sobre los que tengas permiso de realizar escaneos de red.
